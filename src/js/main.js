338cb2a9ea45c6841351703cdfb15a01513ca037
Matheus Wendel <48329681+Matheuswrs@users.noreply.github.com>
2020-03-14 19:37:40 -0300

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,77 @@
+const inputMaterias = document.querySelector('#qtdMaterias');
+const form = document.querySelector('.inputsAutomaticos');
+
+function pegaValor() {
+  const qtdMaterias = Number(inputMaterias.value);
+  
+  return qtdMaterias;
+}
+
+function criaInputs() {
+  const inputs = pegaValor();
+  for(let i = 1; i <= inputs; i++) {
+    const section = document.createElement('section');
+    section.setAttribute('class', 'item');
+
+
+    const labelCriado = document.createElement('label');
+    labelCriado.appendChild(document.createTextNode(`Sua ${i}ª nota`));
+    labelCriado.setAttribute('for', `input${i}`);
+
+    const inputCriado = document.createElement('input');
+    inputCriado.setAttribute('id', `input${i}`);
+    inputCriado.setAttribute('onblur', 'pegaValores()');
+    inputCriado.setAttribute('autocomplete', 'off');
+    inputCriado.setAttribute('onkeypress', 'return somenteNumeros(event)');
+
+    section.appendChild(labelCriado);
+    section.appendChild(inputCriado);
+
+    form.appendChild(section);
+  }
+  const botao = document.createElement('button');
+  botao.setAttribute('id', 'botao');
+  botao.setAttribute('type', 'submit');
+  botao.appendChild(document.createTextNode('Enviar'));
+  form.appendChild(botao);
+}
+
+function pegaValores() {
+  const inputs = pegaValor();
+  let notaMaterias = [''];
+  for(let i = 1; i <= inputs; i++) {
+    let valorInputs = document.querySelector(`#input${i}`);
+    notaMaterias[i - 1] = Number(valorInputs.value);
+  }
+  return notaMaterias;
+}
+
+function criaP() {
+  const p = document.createElement('p');
+  return p;
+}
+
+const setResultado = (msg) => {
+  const resultado = document.querySelector('#resultado');
+  resultado.innerHTML = '';
+  const p = criaP();
+  
+  p.innerHTML = msg;
+  resultado.appendChild(p);
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const divisor = pegaValor();
+
+  const valores = pegaValores();
+  let resultado = valores.reduce((anterior, proximo) => anterior + proximo);
+  resultado = resultado / divisor;
+  resultado = resultado.toFixed(2);
+
+  const msg = `Seu coeficiente de aprovação: ${resultado}`;
+
+  setResultado(msg);
+
+  console.log(resultado);
+});
